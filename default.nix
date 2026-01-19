cf481bb2106dea2de6e6a5d8baf29c54022ee0fe
Stu Small <stuart.alan.small@gmail.com>
2026-01-18 22:57:57 -0700

    Update deps and add avgolemono recipe

--- default.nix
+++ default.nix
@@ -1,20 +1,24 @@
 let
-  pkgs = import (
+  rust_overlay = import (
     builtins.fetchGit {
-      name = "nixpkgs-jan-25-2025";
-      url = "https://github.com/nixos/nixpkgs/";
+      name = "rust-overlay-dec-30-2025";
+      url = "https://github.com/oxalica/rust-overlay/";
       ref = "refs/heads/master";
-      rev = "aeba1dd05ab1f729eeac31677abfd39092fd1ee0";
-    }) {};
+      rev = "943d2d0bf1b86267287aff826ebd1138d83113b7";
+    }
+  );
+  pkgs = import (builtins.fetchGit {
+    name = "nixpkgs-jan-18-2026";
+    url = "https://github.com/nixos/nixpkgs/";
+    ref = "refs/heads/nixos-25.11";
+    rev = "72ac591e737060deab2b86d6952babd1f896d7c5";
+  }) { overlays = [ rust_overlay ]; };
+  rust_toolchain = pkgs.rust-bin.fromRustupToolchainFile ./build-md/rust-toolchain.toml;
 in
 pkgs.mkShell {
   buildInputs = [
-    pkgs.cargo
+    rust_toolchain
     pkgs.libwebp
-    pkgs.rustc
-    pkgs.rustfmt
     pkgs.zola
   ];
 }
-
-
